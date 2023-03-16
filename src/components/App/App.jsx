e00e58ebb4c7e1da510bed1fadd4b1a31256f3d2
IrynaTopolna <itopolna2808@gmail.com>
2023-03-16 13:14:02 +0200

    Changes classes to hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { GlobalStyles } from '../GlobalStyles';
 import { nanoid } from 'nanoid';
 import PhoneBookForm from '../PhoneBookForm';
@@ -7,45 +7,35 @@ import Filter from 'components/Filter';
 import Layout from '../Layout';
 import { Title, Title2 } from './App.styled';
 
-class App extends Component {
-  state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+const getContactsFromLocalStorage = () => {
+  const savedContacts = localStorage.getItem('contacts');
+  if (savedContacts !== null) {
+    const parsedContacts = JSON.parse(savedContacts);
+    return parsedContacts;
+  }
+  return [];
+};
+
+export default function App() {
+  const [contacts, setContacts] = useState(getContactsFromLocalStorage);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+    console.log('laaaa');
+  }, [contacts]);
+
+  const getFilteredName = filterName => {
+    setFilter(filterName);
   };
 
-  componentDidMount() {
-    const savedContacts = localStorage.getItem('contacts');
-    if (savedContacts !== null) {
-      const parsedContacts = JSON.parse(savedContacts);
-      this.setState({ contacts: parsedContacts });
-      return;
-    }
-    this.setState({ contacts: [] });
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  filterContacts = contactName => {
-    const contacts = this.state.contacts;
+  const filterContacts = contactName => {
     return contacts.filter(({ name }) =>
       name.toLowerCase().includes(contactName)
     );
   };
 
-  getFilteredName = filterName => {
-    this.setState({ filter: filterName });
-  };
-
-  addContact = values => {
+  const addContact = values => {
     const newContact = {
       id: nanoid(),
       name: values.name,
@@ -53,45 +43,29 @@ class App extends Component {
     };
     console.log(newContact);
 
-    this.setState(prevState => {
-      return {
-        contacts: [...prevState.contacts, newContact],
-      };
-    });
+    setContacts(prevState => [...prevState, newContact]);
   };
 
-  deleteContact = contactId => {
+  const deleteContact = contactId => {
     console.log('deleteContact', contactId);
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(
-          contact => contact.id !== contactId
-        ),
-      };
-    });
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== contactId)
+    );
   };
 
-  render() {
-    const data = this.state;
+  return (
+    <Layout>
+      <Title>My phonebook</Title>
+      <PhoneBookForm onSubmit={addContact} contacts={contacts} />
 
-    return (
-      <Layout>
-        <Title>My phonebook</Title>
-        <PhoneBookForm onSubmit={this.addContact} contacts={data.contacts} />
+      <Title2>My contacts</Title2>
+      <Filter onFilter={getFilteredName} value={filter} />
+      <ContactsList
+        contacts={filter ? filterContacts(filter) : contacts}
+        onDelete={deleteContact}
+      />
 
-        <Title2>My contacts</Title2>
-        <Filter onFilter={this.getFilteredName} value={data.filter} />
-        <ContactsList
-          contacts={
-            data.filter ? this.filterContacts(data.filter) : data.contacts
-          }
-          onDelete={this.deleteContact}
-        />
-
-        <GlobalStyles />
-      </Layout>
-    );
-  }
+      <GlobalStyles />
+    </Layout>
+  );
 }
-
-export default App;
